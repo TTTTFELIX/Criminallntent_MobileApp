e875077671cfeeacbfc1cc5ab96141d950f9be18
TTTTFELIX <snie@cpp.edu>
2023-06-02 19:49:48 -0700

    Criminallntent

---
+++ app/src/main/java/com/example/homework2_criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,10 @@
+package com.example.homework2_criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
